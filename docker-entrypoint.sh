509b26841bbe9884779f53f0e73850aa7a4ce438
Teeh Amaral <contato@teehamaral.me>
2017-03-10 17:39:03 -0300

    Remove migrate command from docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,14 +3,12 @@ set -e
 
 case $1 in
  runserver)
-  cd /rapidpro; python manage.py migrate;python manage.py runserver 0.0.0.0:8000
+  cd /rapidpro; python manage.py runserver 0.0.0.0:8000
   ;;
  uwsgi_server)
-  python manage.py migrate
   uwsgi  --socket app.sock --wsgi-file temba/wsgi.py --chmod-socket=666
   ;;
  supervisor)
-  python manage.py migrate
   /usr/bin/supervisord -n -c /rapidpro/supervisor-app.conf
   ;;
 esac
