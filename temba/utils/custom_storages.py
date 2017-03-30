9e457fa732d9a063e18e996fa6dbfc57dd1508d1
Teeh Amaral <contato@teehamaral.me>
2017-03-30 19:01:01 -0300

    Change s3 boto

--- temba/utils/custom_storages.py
+++ temba/utils/custom_storages.py
@@ -1,15 +1,15 @@
 from django.conf import settings
-from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 __author__ = 'teehamaral'
 
 
-class StaticStorage(S3BotoStorage):
+class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
     bucket_name = settings.AWS_STORAGE_BUCKET_NAME
 
 
-class MediaStorage(S3BotoStorage):
+class MediaStorage(S3Boto3Storage):
     default_acl = 'private'
     querystring_auth = True
     bucket_name = settings.AWS_STORAGE_BUCKET_NAME_MEDIA
