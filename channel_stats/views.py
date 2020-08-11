10d243d41642a30a358a4ccd225ee17e223e05f4
Cloves Oliveira <clovesoliveiracc@gmail.com>
2020-08-11 13:14:37 -0300

    Creating ChannelStats view doc string

--- channel_stats/views.py
+++ channel_stats/views.py
@@ -15,6 +15,79 @@ from .serializers import ContactActiveSerializer, ChannelStatsReadSerializer
 
 
 class ChannelStatsEndpoint(ListAPIMixin, BaseAPIView):
+    """
+    This endpoint shows the statistics of messages sent and received by channel.
+
+    ## List Statistics of Messages by Channels
+
+    A **GET** returns the list of the channels of the statistic of messages sent and received for your organization.
+
+    * **uuid** - UUID of the channel.
+
+    Example:
+
+        GET /api/v2/channel_stats.json
+
+    Response:
+
+        {
+            "count": 1,
+            "next": null,
+            "previous": null,
+            "results": [
+                {
+                    "uuid": "600c1831-7f6a-44a3-b1db-78567500553f",
+                    "name": "Web",
+                    "channel_type": "EX",
+                    "msg_count": 1605,
+                    "ivr_count": 0,
+                    "error_count": 0,
+                    "daily_count": [
+                        {
+                            "name": "Incoming",
+                            "type": "msg",
+                            "data": [
+                                {
+                                "date": "2020-01-01",
+                                "count": 3
+                                }
+                            ]
+                        },
+                        {
+                            "name": "Outgoing",
+                            "type": "msg",
+                            "data": [
+                                {
+                                "date": "2020-01-01",
+                                "count": 3
+                                }
+                            ]
+                        },
+                        {
+                            "name": "Errors",
+                            "type": "error",
+                            "data": [
+                                {
+                                "date": "2020-01-01",
+                                "count": 3
+                                }
+                            ]
+                        }
+                    ],
+                    "monthly_totals": [
+                        {
+                            "month_start": "2020-01-01T00:00:00Z",
+                            "incoming_messages_count": 73,
+                            "outgoing_messages_count": 106,
+                            "incoming_ivr_count": 0,
+                            "outgoing_ivr_count": 0,
+                            "error_count": 0
+                        }
+                    ]
+                }
+            ]
+        }
+    """
     permission = "channels.channel_api"
     model = Channel
     serializer_class = ChannelStatsReadSerializer
