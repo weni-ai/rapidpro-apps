d59bca01f66daffa758b79288002a16b9ad14f9b
PeterAlexandre <petermiguel99@gmail.com>
2020-12-16 10:49:52 -0300

    add analytucs api to contacts

---
+++ analytics_api/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from .views import ContactAnalytics
+
+urlpatterns = [
+    url(r"^analytics_api/contacts/$", ContactAnalyticsEndpoint.as_view(),
+        name="api.v2.analytics_api.contacts")
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=["json", "api"])
