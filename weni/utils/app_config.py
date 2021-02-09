1fd32321be60cd8dfcacc7c746c150b7d1104545
Joao <jcbalmeida@gmail.com>
2021-02-09 00:09:00 -0300

    Allows method update_urlpatterns to choose the urls module to extend

--- weni/utils/app_config.py
+++ weni/utils/app_config.py
@@ -1,19 +1,20 @@
-from temba.api.v2.urls import urlpatterns
 from temba.api.v2.views import ExplorerView
+from importlib import import_module
 
 
-def update_urlpatterns(app_urls):
+def update_urlpatterns(app_urls, urls_module="temba.api.v2.urls"):
     def context_data(cls, **kwargs):
         ctx = context_func(cls, **kwargs)
         added = []
         for url in app_urls:
             view = url.callback.view_class
             view_id = id(view)
-            if view_id not in added:
+            if view_id not in added and hasattr(view, "get_read_explorer"):
                 ctx["endpoints"].append(view.get_read_explorer())
                 added.append(view_id)
         return ctx
 
-    urlpatterns.extend(app_urls)
+    base_urls = import_module(urls_module)
+    base_urls.urlpatterns.extend(app_urls)
     context_func = ExplorerView.get_context_data
     ExplorerView.get_context_data = context_data
