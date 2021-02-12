bfc4b761acadb1df3ba6686d0fa2d42b2b0cf87c
Sandro-Meireles <playploygames@gmail.com>
2021-02-12 18:46:51 -0300

    feat: List users of Org

--- weni/org_grpc/serializers.py
+++ weni/org_grpc/serializers.py
@@ -1,10 +1,28 @@
+from django.contrib.auth.models import User
+
+from rest_framework import serializers
+
 from django_grpc_framework import proto_serializers
 import org_pb2
 
 from temba.orgs.models import Org
 
+
 class OrgProtoSerializer(proto_serializers.ModelProtoSerializer):
+
+    users = serializers.SerializerMethodField()
+
+    def get_users(self, org: Org):
+        users = org.administrators.union(
+            org.viewers.all(),
+            org.editors.all(),
+            org.surveyors.all()
+        )
+        return list(users.values(
+            "id", "email", "username", "first_name", "last_name"
+        ))
+
     class Meta:
         model = Org
         proto_class = org_pb2.Org
-        fields = ['id', 'name', 'uuid']
+        fields = ["id", "name", "uuid", "users"]
