eac9b776461582c2f295acac15eb1475f719d568
Sandro-Meireles <playploygames@gmail.com>
2021-02-12 16:23:05 -0300

    feat: List orgs by user email

---
+++ weni/org_grpc/urls.py
@@ -0,0 +1,6 @@
+import org_pb2_grpc
+from weni.org_grpc.services import OrgService
+
+
+def grpc_handlers(server):
+    org_pb2_grpc.add_OrgControllerServicer_to_server(OrgService.as_servicer(), server)
