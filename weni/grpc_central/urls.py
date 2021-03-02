d792fa2d5e99a079164343d3fecc49c56a3da4a8
Joao <jcbalmeida@gmail.com>
2021-03-01 22:46:43 -0300

    Adds billing grpc_handler to grpc_central

--- weni/grpc_central/urls.py
+++ weni/grpc_central/urls.py
@@ -1,7 +1,8 @@
 from weni.org_grpc.urls import grpc_handlers as org_handlers
 from weni.flow_grpc.urls import grpc_handlers as flow_handlers
-
+from weni.billing.urls import grpc_handlers as billing_handlers
 
 def grpc_handlers(server):
     org_handlers(server)
     flow_handlers(server)
+    billing_handlers(server)
