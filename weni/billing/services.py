c7f05a742b304042bd5887dd7f992bbd51e12c1f
Joao <jcbalmeida@gmail.com>
2021-03-02 18:02:12 -0300

    Calls ActiveContactsQuery.total() from BillingService

--- weni/billing/services.py
+++ weni/billing/services.py
@@ -1,6 +1,8 @@
 from django_grpc_framework import generics
 from weni.billing.grpc_gen.billing_pb2 import BillingResponse
+from weni.billing.queries import ActiveContactsQuery as Query
+
 
 class BillingService(generics.GenericService):
     def Total(self, request, context):
-        return BillingResponse(active_contacts=42)
+        return BillingResponse(active_contacts=Query.total())
