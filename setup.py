e6af5d6e1d16b7af3c6c462f991eaea5b59594f8
Joao <jcbalmeida@gmail.com>
2021-03-01 22:44:51 -0300

    removes duplicated "install_requires" entry

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(name="rapidpro-apps", install_requires=[
     "mozilla-django-oidc",
     "djangogrpcframework",
-    "djangogrpcframework",
     "grpcio",
     "grpcio-tools",
     ])
