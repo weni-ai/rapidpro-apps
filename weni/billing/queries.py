965d013326a44a0ac31e9ed49094d363813d1567
Joao <jcbalmeida@gmail.com>
2021-03-02 17:48:37 -0300

    Initial query for active contacts total

--- weni/billing/queries.py
+++ weni/billing/queries.py
@@ -1,4 +1,14 @@
+from django.db import connection
+
+
 class ActiveContactsQuery:
     @classmethod
     def total(cls):
-        return 42
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "select count(distinct urn.contact_id) from msgs_msg msg "
+                "inner join contacts_contacturn urn on urn.id = msg.contact_urn_id "
+                "where not (msg.status = 'F');"
+            )
+            row = cursor.fetchone()
+            return row[0]
