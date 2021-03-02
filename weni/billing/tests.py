9bf4768a45363ae9a5c567d92e876fec7a96bf10
Joao <jcbalmeida@gmail.com>
2021-03-02 17:26:42 -0300

    Test active contacts totals

--- weni/billing/tests.py
+++ weni/billing/tests.py
@@ -1,4 +1,3 @@
-from django_grpc_framework.test import FakeRpcError, RPCTransactionTestCase
 from temba.tests import TembaTest
 from weni.billing.queries import ActiveContactsQuery
 
@@ -8,5 +7,10 @@ class ActiveContactsQueryTest(TembaTest):
         super().setUp()
         self.query = ActiveContactsQuery
 
+        # create some contacts and incoming msgs
+        for index in range(10):
+            contact = self.create_contact(f"Contact {index+1}", phone=f"+5531248269{index:2d}")
+            self.create_incoming_msg(contact, "Hi there")
+
     def test_total(self):
-        assert self.query.total() == 42
+        self.assertEqual(self.query.total(), 10)
