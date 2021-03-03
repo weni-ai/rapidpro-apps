42f6e7a617df94c655eba6eaa261ce509e51ce13
Joao <jcbalmeida@gmail.com>
2021-03-02 21:18:47 -0300

    Add filter parameters into ActiveContactsQuery

--- weni/billing/queries.py
+++ weni/billing/queries.py
@@ -1,14 +1,29 @@
 from django.db import connection
+from datetime import datetime
 
 
 class ActiveContactsQuery:
     @classmethod
-    def total(cls):
+    def total(cls, org_uuid: str, before: datetime, after: datetime) -> int:
+        """Counts the number of active contacts (Contacts with any message between 'before' and 'after' with status different from FAILED) of a given org.
+
+        Args:
+            org_uuid (str): Org for search contacts
+            before (datetime): Beginning of time range
+            after (datetime): Ending of time range
+
+        Returns:
+            int: Total of active contacts in the given time range.
+        """
         with connection.cursor() as cursor:
             cursor.execute(
                 "select count(distinct urn.contact_id) from msgs_msg msg "
                 "inner join contacts_contacturn urn on urn.id = msg.contact_urn_id "
-                "where not (msg.status = 'F');"
+                "inner join orgs_org org on org.id = msg.org_id "
+                f"where org.uuid = '{org_uuid}' "
+                f"and msg.created_on >= '{after}' "
+                f"and msg.created_on < '{before}' "
+                "and not (msg.status = 'F');"
             )
             row = cursor.fetchone()
             return row[0]
--- weni/billing/tests.py
+++ weni/billing/tests.py
@@ -1,16 +1,36 @@
 from temba.tests import TembaTest
 from weni.billing.queries import ActiveContactsQuery
+from django.utils import timezone as tz
+from django.conf import settings
+
+from temba.orgs.models import Org
 
 
 class ActiveContactsQueryTest(TembaTest):
     def setUp(self):
         super().setUp()
         self.query = ActiveContactsQuery
+        self.start = tz.now()
 
         # create some contacts and incoming msgs
         for index in range(10):
             contact = self.create_contact(f"Contact {index+1}", phone=f"+5531248269{index:2d}")
             self.create_incoming_msg(contact, "Hi there")
 
+        # some other contacts out of time range filter
+        for index in range(10, 15):
+            contact = self.create_contact(f"Contact {index+1}", phone=f"+5531248269{index:2d}")
+            self.create_incoming_msg(contact, "Hi there", created_on=self.start - tz.timedelta(minutes=10))
+
+        self.another_org = Org.objects.create(
+            name="Weni",
+            timezone=tz.pytz.timezone("America/Sao_Paulo"),
+            brand=settings.DEFAULT_BRAND,
+            created_by=self.user,
+            modified_by=self.user,
+        )
+
     def test_total(self):
-        self.assertEqual(self.query.total(), 10)
+        self.assertEqual(self.query.total(self.org.uuid, tz.now(), self.start), 10)
+        self.assertEqual(self.query.total(self.org.uuid, tz.now(), self.start - tz.timedelta(minutes=11)), 15)
+        self.assertEqual(self.query.total(self.another_org.uuid, tz.now(), self.start), 0)
