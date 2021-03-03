d226e6ac012e9c02ea3abac79bd871b821363082
Sandro-Meireles <playploygames@gmail.com>
2021-03-03 10:34:11 -0300

    fix: Filtering active orgs

--- weni/grpc_central/urls.py
+++ weni/grpc_central/urls.py
@@ -1,7 +1,9 @@
 from weni.org_grpc.urls import grpc_handlers as org_handlers
 from weni.flow_grpc.urls import grpc_handlers as flow_handlers
+from weni.user_grpc.urls import grpc_handlers as user_handlers
 
 
 def grpc_handlers(server):
     org_handlers(server)
     flow_handlers(server)
+    user_handlers(server)
