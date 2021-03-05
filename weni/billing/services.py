7984b382268b02319320f3411426a8c0f2ee12e4
Joao <jcbalmeida@gmail.com>
2021-03-05 04:46:13 -0300

    Fixes service request deserialization

--- weni/billing/services.py
+++ weni/billing/services.py
@@ -1,7 +1,15 @@
 from django_grpc_framework import generics
 from weni.billing.grpc_gen.billing_pb2 import BillingResponse
 from weni.billing.queries import ActiveContactsQuery as Query
+from weni.billing.serializers import BillingRequestSerializer as Serializer
+
 
 class BillingService(generics.GenericService):
     def Total(self, request, context):
-        return BillingResponse(active_contacts=Query.total(request.org_uuid, request.before, request.after))
+        serializer = Serializer(message=request)
+        if serializer.is_valid():
+            org_uuid = serializer.validated_data["org_uuid"]
+            before = serializer.validated_data["before"]
+            after = serializer.validated_data["after"]
+            total_count = Query.total(org_uuid, before, after)
+            return BillingResponse(active_contacts=total_count)
