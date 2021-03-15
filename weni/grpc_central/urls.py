431362e57991c1aa092f6a26ec1c2f6d652171e8
João Carlos <jcbalmeida@users.noreply.github.com>
2021-03-15 13:36:10 -0300

    Feature/weni billing (#20)
    * Adds django-grpc as requirement
    
    * removes duplicated "install_requires" entry
    
    * Implements billing grpc service
    
    * Adds billing grpc_handler to grpc_central
    
    * Includes pb2 files

--- weni/grpc_central/urls.py
+++ weni/grpc_central/urls.py
@@ -1,5 +1,6 @@
-from weni.org_grpc.urls import grpc_handlers as org_handlers
+from weni.billing.urls import grpc_handlers as billing_handlers
 from weni.flow_grpc.urls import grpc_handlers as flow_handlers
+from weni.org_grpc.urls import grpc_handlers as org_handlers
 from weni.statistic_grpc.urls import grpc_handlers as statistic_handlers
 from weni.user_grpc.urls import grpc_handlers as user_handlers
 
@@ -7,5 +8,6 @@ from weni.user_grpc.urls import grpc_handlers as user_handlers
 def grpc_handlers(server):
     org_handlers(server)
     flow_handlers(server)
+    billing_handlers(server)
     statistic_handlers(server)
     user_handlers(server)
