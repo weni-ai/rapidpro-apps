0bf4b01c9af800e317ab67ccd1db39011a5b5b7a
Joao <jcbalmeida@gmail.com>
2021-03-19 13:27:00 -0300

    Adds org_choose view to weni.auth.urls

--- weni/auth/urls.py
+++ weni/auth/urls.py
@@ -1,5 +1,12 @@
-from .views import check_user_legacy
 from django.conf.urls import include, url
+from weni.auth.views import check_user_legacy, org_choose
 
-urlpatterns = [url(r"^oidc/", include("mozilla_django_oidc.urls"))]
-urlpatterns += [url(r"^check-user-legacy/(?P<email>.*\\w+)/$", check_user_legacy, name="check-user-legacy")]
+urlpatterns = [
+    url(r"^oidc/", include("mozilla_django_oidc.urls")),
+    url(r"^check-user-legacy/(?P<email>.*\\w+)/$", check_user_legacy, name="check-user-legacy"),
+    url(
+        r"^weni/org_choose/(?P<organization>[0-9a-fA-F]{8}-?)[0-9a-fA-F]{4}-?){3}[0-9a-fA-F]{12}",
+        org_choose,
+        name="weni-org-choose",
+    ),
+]
