a09b678dcf33a002e26beec38bf4ccafdff127c6
Joao <jcbalmeida@gmail.com>
2021-03-23 09:52:37 -0300

    Fix /weni/authenticate/<org> url

--- weni/auth/urls.py
+++ weni/auth/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r"^oidc/", include("mozilla_django_oidc.urls")),
     url(r"^check-user-legacy/(?P<email>.*\\w+)/$", check_user_legacy, name="check-user-legacy"),
     url(
-        r"^weni/authenticate?(?P<organization>[0-9a-fA-F]{8}-?([0-9a-fA-F]{4}-?){3}[0-9a-fA-F]{12})",
+        r"^weni/authenticate/(?P<organization>[0-9a-fA-F]{8}-?([0-9a-fA-F]{4}-?){3}[0-9a-fA-F]{12})",
         WeniAuthenticationRequestView.as_view(),
         name="weni-org-choose",
     ),
