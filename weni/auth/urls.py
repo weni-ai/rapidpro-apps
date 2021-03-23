d87f1ddc4761e367891babec83918e70747a0059
Joao <jcbalmeida@gmail.com>
2021-03-23 13:56:40 -0300

    Fixes weni-org-choose url path

--- weni/auth/urls.py
+++ weni/auth/urls.py
@@ -1,13 +1,10 @@
 from django.conf.urls import include, url
+from django.urls import path
 
 from weni.auth.views import check_user_legacy, WeniAuthenticationRequestView
 
 urlpatterns = [
     url(r"^oidc/", include("mozilla_django_oidc.urls")),
     url(r"^check-user-legacy/(?P<email>.*\\w+)/$", check_user_legacy, name="check-user-legacy"),
-    url(
-        r"^weni/authenticate/(?P<organization>[0-9a-fA-F]{8}-?([0-9a-fA-F]{4}-?){3}[0-9a-fA-F]{12})",
-        WeniAuthenticationRequestView.as_view(),
-        name="weni-org-choose",
-    ),
+    path("weni/authenticate/<uuid:organization>", WeniAuthenticationRequestView.as_view(), name="weni-org-choose",),
 ]
