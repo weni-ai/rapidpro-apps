0b2466d74fec18342ba6869872786df5a5bd5c52
João Carlos <jcbalmeida@users.noreply.github.com>
2021-04-08 15:40:54 -0300

    Adds endpoints to flow editor and org settings, changing current org (#40)
    * Creates weni.auth.org_choose decorator
    
    * Changes url pattern to /weni/<org_uuid>/authenticate;
    
    * Refactors WeniAuthenticationRequestView to use weni.auth.decorators.org_choose;
    * Implements weni.auth.views.OrgHomeRedirectView (redirects to temba's "/org/home" view, setting the org in the url /weni/<org_uuid>)
    
    * Adds weni.auth.views.FlowEditorRedirectView

--- weni/auth/urls.py
+++ weni/auth/urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls import include, url
 from django.urls import path
 
-from weni.auth.views import check_user_legacy, WeniAuthenticationRequestView
+from weni.auth.views import (
+    check_user_legacy,
+    WeniAuthenticationRequestView,
+    OrgHomeRedirectView,
+    FlowEditorRedirectView,
+)
 
 urlpatterns = [
     url(r"^oidc/", include("mozilla_django_oidc.urls")),
     url(r"^check-user-legacy/(?P<email>.*\\w+)/$", check_user_legacy, name="check-user-legacy"),
-    path("weni/authenticate/<uuid:organization>", WeniAuthenticationRequestView.as_view(), name="weni-org-choose",),
+    path("weni/<uuid:organization>/authenticate", WeniAuthenticationRequestView.as_view(), name="weni-authenticate",),
+    path(
+        "weni/<uuid:organization>/flow/<uuid:uuid>/editor", FlowEditorRedirectView.as_view(), name="weni-flow-editor",
+    ),
+    path("weni/<uuid:organization>/config", OrgHomeRedirectView.as_view(), name="weni-org-home"),
 ]
