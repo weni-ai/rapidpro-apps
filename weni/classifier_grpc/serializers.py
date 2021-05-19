48bfd039b9abf5c39880e5c33da1f3c8f809d75b
Sandro Meireles <49874732+Sandro-Meireles@users.noreply.github.com>
2021-05-19 11:39:02 -0300

    feat: Added access_token to create and list classifier (#43)

--- weni/classifier_grpc/serializers.py
+++ weni/classifier_grpc/serializers.py
@@ -11,13 +11,20 @@ class ClassifierProtoSerializer(proto_serializers.ModelProtoSerializer):
     uuid = serializers.UUIDField(read_only=True)
     classifier_type = serializers.CharField(required=True)
     name = serializers.CharField(required=True)
+    access_token = weni_serializers.SerializerMethodCharField(required=True)
     user = weni_serializers.UserEmailRelatedField(write_only=True)
     org = weni_serializers.OrgUUIDRelatedField(write_only=True)
 
+    def get_access_token(self, instance):
+        return instance.config.get("access_token")
+
     def create(self, validated_data: dict) -> Classifier:
-        return Classifier.create(config={}, **validated_data)
+        config = dict(access_token=validated_data["access_token"])
+        validated_data.pop("access_token")
+
+        return Classifier.create(config=config, **validated_data, sync=False)
 
     class Meta:
         model = Classifier
         proto_class = classifier_pb2.Classifier
-        fields = ["uuid", "classifier_type", "name", "org", "user"]
+        fields = ["uuid", "classifier_type", "name", "access_token", "org", "user"]
--- weni/classifier_grpc/tests.py
+++ weni/classifier_grpc/tests.py
@@ -11,6 +11,8 @@ from weni.classifier_grpc.grpc_gen import classifier_pb2, classifier_pb2_grpc
 
 class ClassifierServiceTest(RPCTransactionTestCase):
     def setUp(self):
+        self.config = {"access_token": "hbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"}
+
         self.admin = User.objects.create_user(
             username="testuser", password="123", email="test@weni.ai", is_superuser=True
         )
@@ -25,7 +27,7 @@ class ClassifierServiceTest(RPCTransactionTestCase):
         org = Org.objects.first()
         org_uuid = str(org.uuid)
 
-        classifier = Classifier.create(org, self.admin, WitType.slug, "Booker", {}, sync=False)
+        classifier = Classifier.create(org, self.admin, WitType.slug, "Booker", self.config, sync=False)
 
         response = self.classifier_list_request(org_uuid=org_uuid)
         messages = [message for message in response]
@@ -37,8 +39,9 @@ class ClassifierServiceTest(RPCTransactionTestCase):
         self.assertEqual(message.name, "Booker")
         self.assertEqual(message.classifier_type, WitType.slug)
         self.assertEqual(message.uuid, str(classifier.uuid))
+        self.assertEqual(message.access_token, self.config["access_token"])
 
-        classifier = Classifier.create(org, self.admin, LuisType.slug, "Test", {}, sync=False)
+        classifier = Classifier.create(org, self.admin, LuisType.slug, "Test", self.config, sync=False)
 
         response = self.classifier_list_request(org_uuid=org_uuid)
         messages = [message for message in response]
@@ -50,6 +53,7 @@ class ClassifierServiceTest(RPCTransactionTestCase):
         self.assertEqual(message.name, "Test")
         self.assertEqual(message.classifier_type, LuisType.slug)
         self.assertEqual(message.uuid, str(classifier.uuid))
+        self.assertEqual(message.access_token, self.config["access_token"])
 
         response = self.classifier_list_request(org_uuid=org_uuid, classifier_type=LuisType.slug)
         messages = [message for message in response]
@@ -61,8 +65,9 @@ class ClassifierServiceTest(RPCTransactionTestCase):
         self.assertEqual(message.name, "Test")
         self.assertEqual(message.classifier_type, LuisType.slug)
         self.assertEqual(message.uuid, str(classifier.uuid))
+        self.assertEqual(message.access_token, self.config["access_token"])
 
-        classifier = Classifier.create(org, self.admin, LuisType.slug, "Test2", {}, sync=False)
+        classifier = Classifier.create(org, self.admin, LuisType.slug, "Test2", self.config, sync=False)
 
         response = self.classifier_list_request(org_uuid=org_uuid, classifier_type=LuisType.slug)
         messages = [message for message in response]
@@ -77,6 +82,7 @@ class ClassifierServiceTest(RPCTransactionTestCase):
     def test_create_classifier(self):
         org = Org.objects.first()
         user = self.admin
+        access_token = self.config["access_token"]
 
         name = "Test Name"
         classifier_type = "Test Type"
@@ -85,11 +91,13 @@ class ClassifierServiceTest(RPCTransactionTestCase):
             classifier_type=classifier_type,
             user=user.email,
             org=str(org.uuid),
-            name=name
+            name=name,
+            access_token=access_token
         )
 
         self.assertEqual(response.name, name)
         self.assertEqual(response.classifier_type, classifier_type)
+        self.assertEqual(response.access_token, access_token)
 
     def classifier_list_request(self, **kwargs):
         return self.stub.List(classifier_pb2.ClassifierListRequest(**kwargs))
