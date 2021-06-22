58c1975928d3035e8098fc05952e2c1eb821dff7
Joao <jcbalmeida@gmail.com>
2021-06-22 20:38:35 -0300

    set mozilla-django-oidc as dependency from source (github.com/mozilla/mozilla-django-oidc)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="rapidpro-apps", install_requires=[
-    "mozilla-django-oidc",
+    "mozilla-django-oidc@git+https://github.com/mozilla/mozilla-django-oidc.git@master",
     "djangogrpcframework",
     "grpcio",
     "grpcio-tools",
