7497b5bb7f428e47d656d511cdcc5033bea24ff4
Joao <jcbalmeida@gmail.com>
2021-08-09 02:11:01 -0300

    Fix grpc handlers references

--- weni/grpc/core/urls.py
+++ weni/grpc/core/urls.py
@@ -1,9 +1,9 @@
-from weni.billing.urls import grpc_handlers as billing_handlers
-from weni.flow_grpc.urls import grpc_handlers as flow_handlers
-from weni.org_grpc.urls import grpc_handlers as org_handlers
-from weni.statistic_grpc.urls import grpc_handlers as statistic_handlers
-from weni.user_grpc.urls import grpc_handlers as user_handlers
-from weni.classifier_grpc.urls import grpc_handlers as classifier_handlers
+from weni.grpc.billing.urls import grpc_handlers as billing_handlers
+from weni.grpc.flow.urls import grpc_handlers as flow_handlers
+from weni.grpc.org.urls import grpc_handlers as org_handlers
+from weni.grpc.statistic.urls import grpc_handlers as statistic_handlers
+from weni.grpc.user.urls import grpc_handlers as user_handlers
+from weni.grpc.classifier.urls import grpc_handlers as classifier_handlers
 
 
 def grpc_handlers(server):
