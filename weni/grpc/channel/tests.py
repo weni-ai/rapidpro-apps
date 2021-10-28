ab3af21b20708574040c5a3be19fee7f6b53f081
Sandro Meireles <49874732+Sandro-Meireles@users.noreply.github.com>
2021-10-28 16:16:43 -0300

    feat: Add channel release endpoint (#76)

--- weni/grpc/channel/tests.py
+++ weni/grpc/channel/tests.py
@@ -10,21 +10,51 @@ from weni.protobuf.flows import channel_pb2, channel_pb2_grpc
 User = get_user_model()
 
 
-class WeniWebChatCreateServiceTest(RPCTransactionTestCase):
+class gRPCClient:
+    def channel_create_request(self, **kwargs):
+        stub = channel_pb2_grpc.WeniWebChatControllerStub(self.channel)
+        return stub.Create(channel_pb2.WeniWebChatCreateRequest(**kwargs))
+
+    def channel_relesase_request(self, **kwargs):
+        stub = channel_pb2_grpc.ChannelControllerStub(self.channel)
+        return stub.Destroy(channel_pb2.ChannelDestroyRequest(**kwargs))
+
+
+class WeniWebChatCreateServiceTest(gRPCClient, RPCTransactionTestCase):
     def setUp(self):
         self.user = User.objects.create_user(username="testuser", password="123", email="test@weni.ai")
-        self.org = Org.objects.create(name="Weni", timezone="Africa/Kigali", created_by=self.user, modified_by=self.user)
+        self.org = Org.objects.create(
+            name="Weni", timezone="Africa/Kigali", created_by=self.user, modified_by=self.user
+        )
 
         super().setUp()
         self.stub = channel_pb2_grpc.WeniWebChatControllerStub(self.channel)
 
     def test_create_weni_web_chat_channel(self):
-        request_data = dict(org=str(self.org.uuid), name="fake wwc", user=self.user.email, base_url="https://dash.weni.ai")
+        request_data = dict(
+            org=str(self.org.uuid), name="fake wwc", user=self.user.email, base_url="https://dash.weni.ai"
+        )
         response = self.channel_create_request(**request_data)
 
         channel = Channel.objects.get(uuid=response.uuid)
         self.assertEqual(channel.created_by, self.user)
         self.assertEqual(channel.config[CONFIG_BASE_URL], request_data["base_url"])
 
-    def channel_create_request(self, **kwargs):
-        return self.stub.Create(channel_pb2.WeniWebChatCreateRequest(**kwargs))
+
+class ReleaseChannelServiceTest(gRPCClient, RPCTransactionTestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="testuser", password="123", email="test@weni.ai")
+        self.org = Org.objects.create(
+            name="Weni", timezone="Africa/Kigali", created_by=self.user, modified_by=self.user
+        )
+
+        super().setUp()
+
+        response = self.channel_create_request(
+            org=str(self.org.uuid), name="fake wwc", user=self.user.email, base_url="https://dash.weni.ai"
+        )
+        self.channel_obj = Channel.objects.get(uuid=response.uuid)
+
+    def test_alg(self):
+        self.channel_relesase_request(uuid=self.channel_obj.uuid, user=self.user.email)
+        self.assertFalse(Channel.objects.get(id=self.channel_obj.id).is_active)
--- weni/grpc/channel/urls.py
+++ weni/grpc/channel/urls.py
@@ -3,4 +3,5 @@ from weni.grpc.channel import services
 
 
 def grpc_handlers(server):
+    channel_pb2_grpc.add_ChannelControllerServicer_to_server(services.ChannelService.as_servicer(), server)
     channel_pb2_grpc.add_WeniWebChatControllerServicer_to_server(services.WeniWebChatService.as_servicer(), server)
