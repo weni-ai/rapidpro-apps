0e01fc2b500db3e0fca35afb8a88169f9394205b
Sandro Meireles <49874732+Sandro-Meireles@users.noreply.github.com>
2022-02-21 18:56:06 -0300

    Reverts unnecessary billing commits (#101)
    * Revert "fix: Refactor some billing endpoint nomenclatures and validate organization (#92)"
    
    This reverts commit e5abb95633d8ffe254eb5bca87bd932548439129.
    
    * Revert "fix: Refactors the billing query (#93)"
    
    This reverts commit ee105fea14d0075fa50d0ddf1ed577a26454b622.

--- weni/grpc/billing/urls.py
+++ weni/grpc/billing/urls.py
@@ -3,4 +3,4 @@ from weni.grpc.billing.services import BillingService
 
 
 def grpc_handlers(server):
-    billing_pb2_grpc.add_BillingControllerServicer_to_server(BillingService.as_servicer(), server)
+    billing_pb2_grpc.add_BillingServicer_to_server(BillingService.as_servicer(), server)
