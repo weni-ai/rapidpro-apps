cdc9d00796b95a318497e5e0876b27bdb21c5389
Sandro Meireles <49874732+Sandro-Meireles@users.noreply.github.com>
2022-04-18 17:38:12 -0300

    Revert "Fix/is active (#109)"
    This reverts commit 8f0a89bfc462f7dd047b68ba4b780ad8a2cc99da.

--- weni/grpc/org/serializers.py
+++ weni/grpc/org/serializers.py
@@ -64,5 +64,4 @@ class OrgUpdateProtoSerializer(proto_serializers.ModelProtoSerializer):
             "is_multi_user",
             "is_multi_org",
             "is_suspended",
-            "is_active"
         ]
