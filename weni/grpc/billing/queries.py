9170faf6e30658e017a4ba91f65a04127802a9c0
Sandro Meireles <49874732+Sandro-Meireles@users.noreply.github.com>
2022-04-19 14:20:32 -0300

    Revert "fix: Refactors the billing query (#93)" (#115)
    This reverts commit ee105fea14d0075fa50d0ddf1ed577a26454b622.

--- weni/grpc/billing/queries.py
+++ weni/grpc/billing/queries.py
@@ -1,36 +1,10 @@
 from datetime import datetime
 
 from django.db import connection
-from django.db.models import OuterRef, Subquery, Q
+from django.db.models import OuterRef, Subquery
 from temba.contacts.models import Contact
 from temba.msgs.models import Msg
 from temba.orgs.models import Org
-
-
-TOTAL_SQL = """
-SELECT
-	COUNT(DISTINCT urn.contact_id)
-FROM contacts_contacturn AS urn
-INNER JOIN orgs_org AS org
-	ON org.id = urn.org_id
-WHERE EXISTS (
-	SELECT 1 FROM msgs_msg AS msg
-	INNER JOIN channels_channel AS channel
-		ON channel.id = msg.channel_id
-	WHERE org.uuid = '%s'
-    AND msg.created_on BETWEEN '%s' AND '%s'
-	AND NOT (
-		channel.channel_type IN ('EX', 'WWC') AND (
-            (
-                msg.text IN ('start','','pushinho')
-                AND msg.direction = 'I'
-            ) OR msg.direction = 'O'
-        )
-	)
-	AND msg.contact_urn_id = urn.id
-    AND NOT (msg.status = 'F')
-);
-"""
 
 
 class ActiveContactsQuery:
@@ -47,7 +21,15 @@ class ActiveContactsQuery:
             int: Total of active contacts in the given time range.
         """
         with connection.cursor() as cursor:
-            cursor.execute(TOTAL_SQL % (org_uuid, after, before))
+            cursor.execute(
+                "select count(distinct urn.contact_id) from msgs_msg msg "
+                "inner join contacts_contacturn urn on urn.id = msg.contact_urn_id "
+                "inner join orgs_org org on org.id = msg.org_id "
+                f"where org.uuid = '{org_uuid}' "
+                f"and msg.created_on >= '{after}' "
+                f"and msg.created_on < '{before}' "
+                "and not (msg.status = 'F');"
+            )
             row = cursor.fetchone()
             return row[0]
 
@@ -57,8 +39,6 @@ class ActiveContactsQuery:
         msg = (
             Msg.objects.filter(contact_urn__contact__pk=OuterRef("pk"), created_on__lte=before, created_on__gte=after)
             .exclude(status="F")
-            .exclude(channel__channel_type__in=("EX", "WWC"), direction="O")
-            .exclude(channel__channel_type__in=("EX", "WWC"), direction="I", text__in=("start", "", "pushinho"))
             .order_by("-created_on")
         )
 
@@ -71,10 +51,7 @@ class ActiveContactsQuery:
                 channel__uuid=Subquery(msg.values("channel__uuid")[:1]),
                 channel__name=Subquery(msg.values("channel__name")[:1]),
             )
-            .filter(
-                org=org,
-                msg__uuid__isnull=False,
-            )
+            .filter(org=org, msg__uuid__isnull=False,)
             .values(
                 "uuid",
                 "name",
@@ -83,6 +60,6 @@ class ActiveContactsQuery:
                 "msg__sent_on",
                 "msg__direction",
                 "channel__uuid",
-                "channel__name",
+                "channel__name"
             )
         )
