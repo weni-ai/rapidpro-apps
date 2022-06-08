220021de0e1f13e2a8ce53a03a11516e788f4cdd
Sandro Meireles <sandro.meirelesdev@gmail.com>
2022-06-08 20:03:05 -0300

    fix: Adjust WhatsApp Cloud create method

--- weni/grpc/channel/serializers.py
+++ weni/grpc/channel/serializers.py
@@ -3,11 +3,11 @@ import json
 from django_grpc_framework import proto_serializers
 from rest_framework import serializers
 from django.core.validators import URLValidator
-from google.protobuf.empty_pb2 import Empty
 
 from temba.channels.models import Channel
 from temba.utils.fields import validate_external_url
 from temba.channels.types.weniwebchat.type import CONFIG_BASE_URL
+from temba.utils import analytics
 from weni.grpc.core import serializers as weni_serializers
 from weni.protobuf.flows import channel_pb2
 from weni.grpc.channel import fields
@@ -83,17 +83,28 @@ class ChannelWACSerializer(proto_serializers.ModelProtoSerializer):
         return json.dumps(instance)
 
     def create(self, validated_data):
-        config = validated_data.get("config")
+        channel_type = Channel.get_type_from_code(channel_type)
+        schemes = channel_type.schemes
 
-        user = validated_data["user"]
-        phone_number_id = validated_data["phone_number_id"]
+        org = validated_data.get("org")
+        name = validated_data.get("name")
+        phone_number_id = validated_data.get("phone_number_id")
+        config = validated_data.get("config", {})
+        user = validated_data.get("user")
 
-        return Channel.create(
-            validated_data["org"],
-            validated_data["user"],
-            None,
-            "WAC",
-            config=config,
-            name=config.get("wa_verified_name"),
+        channel = Channel.objects.create(
+            org=org,
+            country=None,
+            channel_type=channel_type.code,
+            name=name,
             address=phone_number_id,
+            config=config,
+            role=Channel.DEFAULT_ROLE,
+            schemes=schemes,
+            created_by=user,
+            modified_by=user,
         )
+
+        analytics.track(user, "temba.channel_created", dict(channel_type=channel_type.code))
+
+        return channel
