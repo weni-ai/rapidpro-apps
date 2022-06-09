e7714a65f84774413a40014a40d6821ab7a2f2b5
Paulo Abreu <pauloabreucoder@gmail.com>
2022-06-09 12:20:23 -0300

    Feature/billing message detail endpoint (#119)
    * Add endpoint: contact incoming message
    
    * revert tests
    
    * fix issues
    
    * fix import issue
    
    * refactor incoming_message function
    
    * refactor incoming_message function
    
    * specifie last_incoming_message return
    
    * add tests
    
    * change serializers/services and queries to return channel_id and channel_type
    
    * change all IncomingMessages references to MessageDetail
    
    * change version
    
    * fix/run-black
    
    * update weni-protobuffers version and remove alpha from rp-apps version

--- weni/grpc/billing/services.py
+++ weni/grpc/billing/services.py
@@ -2,7 +2,13 @@ from django_grpc_framework import generics
 
 from weni.protobuf.flows.billing_pb2 import TotalResponse
 from weni.grpc.billing.queries import ActiveContactsQuery as Query
-from weni.grpc.billing.serializers import BillingRequestSerializer, ActiveContactDetailSerializer
+from weni.grpc.billing.queries import MessageDetailQuery
+from weni.grpc.billing.serializers import (
+    BillingRequestSerializer,
+    ActiveContactDetailSerializer,
+    MsgDetailSerializer,
+    MessageDetailRequestSerializer,
+)
 
 
 class BillingService(generics.GenericService):
@@ -31,3 +37,18 @@ class BillingService(generics.GenericService):
 
         for message in ActiveContactDetailSerializer(results, many=True).message:
             yield message
+
+    def MessageDetail(self, request, context):
+        serializer = MessageDetailRequestSerializer(message=request)
+        serializer.is_valid(raise_exception=True)
+
+        org_uuid = serializer.validated_data["org_uuid"]
+        contact_uuid = serializer.validated_data["contact_uuid"]
+        before = serializer.validated_data["before"]
+        after = serializer.validated_data["after"]
+
+        msg = MessageDetailQuery.incoming_message(org_uuid, contact_uuid, before, after)
+
+        msg_serializer = MsgDetailSerializer(msg)
+
+        return msg_serializer.message
