55d6f482df0985b5fac9ceacca371e514ee538fb
Sandro Meireles <sandro.meirelesdev@gmail.com>
2022-09-06 16:42:59 -0300

    feat: Adds new internal users app to internal routes

--- weni/internal/urls.py
+++ weni/internal/urls.py
@@ -12,10 +12,12 @@ from django.urls import path, include
 
 from weni.internal.orgs.urls import urlpatterns as orgs_urls
 from weni.internal.flows.urls import urlpatterns as flows_urls
+from weni.internal.users.urls import urlpatterns as users_urls
 
 
 internal_urlpatterns = []
 internal_urlpatterns += orgs_urls
 internal_urlpatterns += flows_urls
+internal_urlpatterns += users_urls
 
 urlpatterns = [path("internals/", include(internal_urlpatterns))]
