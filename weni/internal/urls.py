abceb359fcad8a885351e6b5a09d4431782df84e
Sandro Meireles <49874732+Sandro-Meireles@users.noreply.github.com>
2022-09-15 13:55:57 -0300

    Org and User APIToken internal endpoint (#151)
    * feat: Create User's internal api app
    feat: Add endpoint that allows retrieving API token from specified org and user
    
    * feat: Adds new internal users app to internal routes

---
+++ weni/internal/urls.py
@@ -0,0 +1,23 @@
+"""
+Internal route registration
+
+To register a new app, import it as:
+    from weni.internal.orgs.urls import urlpatterns as orgs_urls
+
+After that concatenate its value in internal_urlpatterns like:
+    internal_urlpatterns += orgs_urls
+"""
+
+from django.urls import path, include
+
+from weni.internal.orgs.urls import urlpatterns as orgs_urls
+from weni.internal.flows.urls import urlpatterns as flows_urls
+from weni.internal.users.urls import urlpatterns as users_urls
+
+
+internal_urlpatterns = []
+internal_urlpatterns += orgs_urls
+internal_urlpatterns += flows_urls
+internal_urlpatterns += users_urls
+
+urlpatterns = [path("internals/", include(internal_urlpatterns))]
