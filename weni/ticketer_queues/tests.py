df66384397e87fd11347924a0c236c7d0714274b
Rafael Soares <rafael.dvlpr@gmail.com>
2022-09-29 15:53:10 -0300

    apply black with --line-length=119 on weni/ticketer_queues

--- weni/ticketer_queues/tests.py
+++ weni/ticketer_queues/tests.py
@@ -1,12 +1,11 @@
-from temba.tests import TembaTest
-from temba.tickets.models import Ticketer, Topic
 from django.utils.http import urlencode
 from django.urls import reverse
 from uuid import uuid4
 from abc import ABC
 from django.contrib.auth.models import Group
 
-
+from temba.tests import TembaTest
+from temba.tickets.models import Ticketer, Topic
 from weni.internal.models import TicketerQueue
 from temba.tickets.types.mailgun.type import MailgunType
 from temba.api.models import APIToken
@@ -19,9 +18,7 @@ class RequestMixin(ABC):
             urlencode(query_params),
         )
 
-        token = APIToken.get_or_create(
-            self.org, self.admin, Group.objects.get(name="Administrators")
-        )
+        token = APIToken.get_or_create(self.org, self.admin, Group.objects.get(name="Administrators"))
 
         return self.client.get(
             url,
@@ -35,9 +32,7 @@ class TicketerQueuesTest(TembaTest, RequestMixin):
         self.fake_chats_uuid = uuid4()
 
         super().setUp()
-        self.ticketer = Ticketer.create(
-            self.org, self.user, MailgunType.slug, "Email (bob@acme.com)", {}
-        )
+        self.ticketer = Ticketer.create(self.org, self.user, MailgunType.slug, "Email (bob@acme.com)", {})
         self.queue = TicketerQueue.objects.create(
             created_by=self.user,
             modified_by=self.user,
