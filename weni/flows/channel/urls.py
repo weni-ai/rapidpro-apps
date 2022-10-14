78cff42dcdea1347e12adbcd60a1fc9737db8beb
Eliton <eliton-jorge@hotmail.com>
2022-10-14 12:57:01 -0300

    feat
    added available channels url

--- weni/flows/channel/urls.py
+++ weni/flows/channel/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework import routers
 
-from .views import ChannelEndpoint
+from .views import ChannelEndpoint, AvailableChannels
 
 router = routers.SimpleRouter()
 router.register("flows-backend/channel", ChannelEndpoint, basename="api.v2.flows_backend.channel")
+router.register("flows-backend/channels", AvailableChannels, basename="api.v2.flows_backend.channels")
 
 urlpatterns = format_suffix_patterns(router.urls, allowed=["json", "api"])
