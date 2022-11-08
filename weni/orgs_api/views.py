ad7ab0fd425fffa01c869f92c44275112d9dba3b
Paulo Abreu <pauloabreucoder@gmail.com>
2022-11-08 15:52:23 -0300

    fix: move is_suspended from config to var

--- weni/orgs_api/views.py
+++ weni/orgs_api/views.py
@@ -17,10 +17,10 @@ class OrgViewSet(GenericViewSet):
     def is_suspended(self, request, uuid=None):
         org = get_object_or_404(Org, uuid=uuid)
 
-        org.config["is_suspended"] = bool(request.data.get("is_suspended"))
+        org.is_suspended = bool(request.data.get("is_suspended"))
         org.save()
 
-        return JsonResponse(dict(is_suspended=org.config.get("is_suspended", False)))
+        return JsonResponse(dict(is_suspended=org.is_suspended))
 
     @action(detail=True, methods=["POST", "DELETE"])
     def suspend_flag(self, request, uuid=None):
