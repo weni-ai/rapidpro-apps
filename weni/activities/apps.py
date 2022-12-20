5a19439bd56004bb68c9d1b0ccb5dcf11b21533c
Sandro Meireles <sandro.meirelesdev@gmail.com>
2022-12-20 18:04:02 -0300

    feat: Creates signals that notify the connect of creation and change in Flow, Channel, Trigger and Campaign models

--- weni/activities/apps.py
+++ weni/activities/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ActivitiesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "weni.activities"
+
+    def ready(self) -> None:
+        from weni.activities import signals
---
+++ weni/activities/signals.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from temba.channels.models import Channel
+from temba.flows.models import Flow
+from temba.triggers.models import Trigger
+from temba.campaigns.models import Campaign
+from weni.activities import tasks
+
+
+def create_recent_activity(instance: models.Model, created: bool):
+    action = "create" if created else "update"
+
+    tasks.create_recent_activity.delay(
+        action=action,
+        entity=instance.__class__.__name__.lower(),
+        entity_name=instance.name,
+        user=instance.modified_by.email,
+        flow_organization=str(instance.org.uuid),
+    )
+
+
+@receiver(post_save, sender=Channel)
+def channel_recent_activity_signal(sender, instance: Channel, created: bool, **kwargs):
+    create_recent_activity(instance, created)
+
+
+@receiver(post_save, sender=Flow)
+def flow_recent_activity_signal(sender, instance: Flow, created: bool, **kwargs):
+    create_recent_activity(instance, created)
+
+
+@receiver(post_save, sender=Trigger)
+def trigger_recent_activity_signal(sender, instance: Trigger, created: bool, **kwargs):
+    create_recent_activity(instance, created)
+
+
+@receiver(post_save, sender=Campaign)
+def campaign_recent_activity_signal(sender, instance: Campaign, created: bool, **kwargs):
+    create_recent_activity(instance, created)
