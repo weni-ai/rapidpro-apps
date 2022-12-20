e3c91c183a82493539db4dbcd500288e672cac4c
elitonzky <elitonzky>
2022-12-20 11:43:11 -0300

    feature: adds RecentActivity model and migration

--- weni/internal/models.py
+++ weni/internal/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 from temba.tickets.models import Ticketer, Topic
+from temba.orgs.models import Org
 
 
 class TicketerQueue(Topic):
@@ -12,3 +14,40 @@ class TicketerQueue(Topic):
 
     def __str__(self):
         return f"Queue[uuid={self.uuid}, name={self.name}]"
+
+
+class RecentActivity(models.Model):
+    UPDATE = "UPDATE"
+    CREATE = "CREATE"
+
+    ACTIONS_CHOICES = (
+        (CREATE, "Entity Created"),
+        (UPDATE, "Updated Entity")
+    )
+
+    FLOW = "FLOW"
+    CHANNEL = "CHANNEL"
+    TRIGGER = "TRIGGER"
+    CAMPAIGN = "CAMPAIGN"
+
+    ENTITY_CHOICES = (
+        (FLOW, "Flow Entity"),
+        (CHANNEL, "Chanell Entity"),
+        (TRIGGER, "Trigger Entity"),
+        (CAMPAIGN, "Campaign Entity")
+    )
+
+    org = models.ForeignKey(Org, on_delete=models.PROTECT)
+    action = models.CharField(max_length=6, choices=ACTIONS_CHOICES)
+    entity = models.CharField(max_length=8, choices=ENTITY_CHOICES)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    entity_name = models.CharField(max_length=255, null=True)
+    created_on = models.DateTimeField()
+
+    class Meta:
+        db_table = "internal_activities_recentactivity"
+
+
+    def __str__(self) -> str:
+        return f"""{self.org} - {self.entity} - {self.entity_name} -
+                   {self.action} - {self.user} - {str(self.created_on)}"""
