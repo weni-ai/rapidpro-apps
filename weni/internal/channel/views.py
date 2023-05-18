44b2f00de365f4d2dd53b686c98ccd0a18d09995
Eliton <53982066+elitonzky@users.noreply.github.com>
2023-05-17 21:32:12 -0300

    fix: release ticketer instance before delete queue

--- weni/internal/channel/views.py
+++ weni/internal/channel/views.py
@@ -8,7 +8,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import viewsets
 from rest_framework import status
-from rest_framework.pagination import PageNumberPagination
 
 from weni.internal.views import InternalGenericViewSet
 from django.conf import settings
@@ -16,7 +15,11 @@ from django.conf import settings
 from temba.channels.models import Channel
 from temba.channels.types import TYPES
 
-from .serializers import ChannelSerializer, CreateChannelSerializer, ChannelWACSerializer
+from .serializers import (
+    ChannelSerializer,
+    CreateChannelSerializer,
+    ChannelWACSerializer,
+)
 
 User = get_user_model()
 
@@ -44,13 +47,17 @@ class ChannelEndpoint(viewsets.ModelViewSet, InternalGenericViewSet):
         except Channel.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-        return JsonResponse(data=self.get_serializer(channel).data, status=status.HTTP_200_OK)
+        return JsonResponse(
+            data=self.get_serializer(channel).data, status=status.HTTP_200_OK
+        )
 
     def create(self, request):
         serializer = CreateChannelSerializer(data=request.data)
 
         if not serializer.is_valid():
-            return JsonResponse(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(
+                data=serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
 
         serializer.save()
 
@@ -69,7 +76,9 @@ class ChannelEndpoint(viewsets.ModelViewSet, InternalGenericViewSet):
         serializer = ChannelWACSerializer(data=request.data)
 
         if not serializer.is_valid():
-            return JsonResponse(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(
+                data=serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
 
         serializer.save()
 
@@ -99,6 +108,7 @@ class AvailableChannels(viewsets.ViewSet, InternalGenericViewSet):
         channel_type = None
         fields_form = {}
         code_type = pk
+        current_form = None
         if code_type:
             channel_type = TYPES.get(code_type.upper(), None)
 
@@ -108,7 +118,12 @@ class AvailableChannels(viewsets.ViewSet, InternalGenericViewSet):
         fields_in_form = []
         if channel_type.claim_view:
             if channel_type.claim_view.form_class:
-                form = channel_type.claim_view.form_class.base_fields
+                current_form = channel_type.claim_view.form_class
+            elif channel_type.claim_view.ClaimForm:
+                current_form = channel_type.claim_view.ClaimForm
+
+            if current_form:
+                form = current_form.base_fields
                 for field in form:
                     fields_in_form.append(extract_form_info(form[field], field))
 
@@ -124,7 +139,10 @@ class AvailableChannels(viewsets.ViewSet, InternalGenericViewSet):
 
             fields_types["attributes"] = attibutes_type
 
-        payload = {"attributes": fields_types.get("attributes"), "form": fields_form.get("form")}
+        payload = {
+            "attributes": fields_types.get("attributes"),
+            "form": fields_form.get("form"),
+        }
 
         return Response(payload)
 
@@ -132,13 +150,25 @@ class AvailableChannels(viewsets.ViewSet, InternalGenericViewSet):
 def extract_type_info(_class):
     channel = {}
     type_exclude = ["<class 'function'>"]
-    items_exclude = ["redact_response_keys", "claim_view_kwargs", "extra_links", "redact_request_keys"]
+    items_exclude = [
+        "redact_response_keys",
+        "claim_view_kwargs",
+        "extra_links",
+        "redact_request_keys",
+    ]
 
     for i in _class.__class__.__dict__.items():
         if not i[0].startswith("_"):
-            if not inspect.isclass(i[1]) and str(type(i[1])) not in (type_exclude) and i[0] not in items_exclude:
+            if (
+                not inspect.isclass(i[1])
+                and str(type(i[1])) not in (type_exclude)
+                and i[0] not in items_exclude
+            ):
                 if str(type(i[1])) == "<enum 'Category'>":
-                    channel[i[0]] = {"name": i[1].name if i[1].name else "", "value": i[1].value if i[1].value else ""}
+                    channel[i[0]] = {
+                        "name": i[1].name if i[1].name else "",
+                        "value": i[1].value if i[1].value else "",
+                    }
 
                 elif i[0] == "configuration_urls":
                     if i[1]:
@@ -153,7 +183,9 @@ def extract_type_info(_class):
                                     url_dict["url"] = str(url.get("url"))
 
                                 if i[1][0].get("description"):
-                                    url_dict["description"] = str(url.get("description"))
+                                    url_dict["description"] = str(
+                                        url.get("description")
+                                    )
 
                             urls_list.append(url_dict)
                             channel[i[0]] = urls_list
@@ -165,11 +197,18 @@ def extract_type_info(_class):
                     channel[i[0]] = str(i[1])
 
                 elif (i[0]) == "ivr_protocol":
-                    channel[i[0]] = {"name": i[1].name if i[1].name else "", "value": i[1].value if i[1].value else ""}
+                    channel[i[0]] = {
+                        "name": i[1].name if i[1].name else "",
+                        "value": i[1].value if i[1].value else "",
+                    }
                 else:
                     channel[i[0]] = i[1]
 
-    if (not (channel.get("code"))) or (not (len(channel)) > 0) or (not (channel.get("name"))):
+    if (
+        (not (channel.get("code")))
+        or (not (len(channel)) > 0)
+        or (not (channel.get("name")))
+    ):
         return None
 
     channel["num_fields"] = len(channel)
@@ -182,13 +221,13 @@ def extract_form_info(_form, name_form):
 
     try:
         detail["type"] = str(_form.widget.input_type)
-    except:
-        detail["type"] = None
+    except AttributeError:
+        detail["type"] = str(_form.widget.__class__.__name__).lower()
 
     if _form.help_text:
         detail["help_text"] = str(_form.help_text)
     else:
-        detail["help_text"] = None
+        detail["help_text"] = ""
 
     if detail.get("type") == "select":
         detail["choices"] = _form.choices
@@ -196,7 +235,7 @@ def extract_form_info(_form, name_form):
     if _form.label:
         detail["label"] = str(_form.label)
     else:
-        detail["label"] = None
+        detail["label"] = ""
 
     if not (detail.get("name")) or not (detail.get("type")):
         return None
--- weni/internal/models.py
+++ weni/internal/models.py
@@ -7,14 +7,23 @@ from temba.orgs.models import Org
 
 
 class TicketerQueue(Topic):
-    topic = models.OneToOneField(Topic, on_delete=models.CASCADE, parent_link=True, related_name="queue")
-    ticketer = models.ForeignKey(Ticketer, on_delete=models.CASCADE, related_name="queues")
+    topic = models.OneToOneField(
+        Topic, on_delete=models.CASCADE, parent_link=True, related_name="queue"
+    )
+    ticketer = models.ForeignKey(
+        Ticketer, on_delete=models.CASCADE, related_name="queues"
+    )
 
     class Meta:
         db_table = "internal_tickets_ticketerqueue"
 
     def __str__(self):
         return f"Queue[uuid={self.uuid}, name={self.name}]"
+
+    def release(self, user):
+        self.ticketer.release(user=user)
+        self.is_active = False
+        self.save()
 
 
 class Project(Org):
