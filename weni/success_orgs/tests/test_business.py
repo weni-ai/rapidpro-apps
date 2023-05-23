6532b31b6f04924a01771e44084782f7283f905a
lucaslinhares <lucaslinhares027@gmail.com>
2023-05-23 10:29:45 -0300

    add has_channel_production in success orgs

--- weni/success_orgs/tests/test_business.py
+++ weni/success_orgs/tests/test_business.py
@@ -23,7 +23,9 @@ class SetupMixin(object):
     def setUp(self) -> None:
         self.user_email = "fake@weni.ai"
         self.user = User.objects.create(email=self.user_email)
-        self.org = Org.objects.create(created_by=self.user, modified_by=self.user, name="fakeorg")
+        self.org = Org.objects.create(
+            created_by=self.user, modified_by=self.user, name="fakeorg"
+        )
 
 
 class GetUserByEmailTestCase(SetupMixin, TestCase):
@@ -43,10 +45,15 @@ class GetUserSuccessOrgsTestCase(SetupMixin, TestCase):
         self.assertTrue(hasattr(org, "has_flows"))
         self.assertTrue(hasattr(org, "has_channel"))
         self.assertTrue(hasattr(org, "has_msg"))
+        self.assertTrue(hasattr(org, "has_channel_production"))
 
     def test_when_adding_classifier_it_returns_true(self):
         Classifier.objects.create(
-            org=self.org, created_by=self.user, modified_by=self.user, config={}, classifier_type="bothub"
+            org=self.org,
+            created_by=self.user,
+            modified_by=self.user,
+            config={},
+            classifier_type="bothub",
         )
 
         org = get_user_success_orgs(self.user).first()
@@ -54,24 +61,34 @@ class GetUserSuccessOrgsTestCase(SetupMixin, TestCase):
         self.assertFalse(org.has_flows)
         self.assertFalse(org.has_channel)
         self.assertFalse(org.has_msg)
+        self.assertFalse(org.has_channel_production)
 
     def test_when_adding_flow_it_returns_true(self):
-        Flow.objects.create(org=self.org, created_by=self.user, modified_by=self.user, saved_by=self.user)
+        Flow.objects.create(
+            org=self.org,
+            created_by=self.user,
+            modified_by=self.user,
+            saved_by=self.user,
+        )
 
         org = get_user_success_orgs(self.user).first()
         self.assertFalse(org.has_ia)
         self.assertTrue(org.has_flows)
         self.assertFalse(org.has_channel)
         self.assertFalse(org.has_msg)
+        self.assertFalse(org.has_channel_production)
 
     def test_when_adding_channel_it_returns_true(self):
-        Channel.objects.create(org=self.org, created_by=self.user, modified_by=self.user)
+        Channel.objects.create(
+            org=self.org, created_by=self.user, modified_by=self.user
+        )
 
         org = get_user_success_orgs(self.user).first()
         self.assertFalse(org.has_ia)
         self.assertFalse(org.has_flows)
         self.assertTrue(org.has_channel)
         self.assertFalse(org.has_msg)
+        self.assertTrue(org.has_channel_production)
 
 
 class RetrieveSuccessOrgTestCase(SetupMixin, TestCase):
