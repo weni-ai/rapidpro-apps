b72d56854c01c84e431b18a16704eb56abc2910a
lucaslinhares <lucaslinhares027@gmail.com>
2023-05-25 18:18:58 -0300

    change create external service to use project over org

--- weni/internal/externals/serializers.py
+++ weni/internal/externals/serializers.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
 
 
-from weni.serializers import OrgUUIDRelatedField, UserEmailRelatedField
+from weni.serializers import UserEmailRelatedField
 from temba.externals.models import ExternalService
+from weni.serializers.fields import ProjectUUIDRelatedField
 
 
 class ExternalServicesSerializer(serializers.Serializer):
-
     uuid = serializers.UUIDField(read_only=True)
     type_code = serializers.CharField(write_only=True)
     type_fields = serializers.JSONField(write_only=True)
-    org = OrgUUIDRelatedField(write_only=True)
+    project = ProjectUUIDRelatedField(write_only=True)
     user = UserEmailRelatedField(write_only=True)
 
     external_service_type = serializers.CharField(read_only=True)
@@ -23,7 +23,7 @@ class ExternalServicesSerializer(serializers.Serializer):
         type_code = validated_data.get("type_code")
         type_fields = validated_data.get("type_fields")
         user = validated_data.get("user")
-        org = validated_data.get("org")
+        project = validated_data.get("project")
 
         try:
             type_ = ExternalService.get_type_from_code(type_code)
@@ -32,4 +32,6 @@ class ExternalServicesSerializer(serializers.Serializer):
 
         type_serializer = type_.serializer_class(data=type_fields)
         type_serializer.is_valid(raise_exception=True)
-        return type_serializer.save(type=type_, created_by=user, modified_by=user, org=org)
+        return type_serializer.save(
+            type=type_, created_by=user, modified_by=user, org=project
+        )
