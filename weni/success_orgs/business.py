2990fd903cab5a09983e21a3489fdf91ea623c04
Lucas <44686243+lucaslinhares@users.noreply.github.com>
2023-06-02 17:49:10 -0300

    add has_channel_production in success orgs (#236)

--- weni/success_orgs/business.py
+++ weni/success_orgs/business.py
@@ -20,10 +20,19 @@ User = get_user_model()
 
 
 SUCCESS_ORG_QUERIES = dict(
-    has_ia=Exists(Classifier.objects.filter(org=OuterRef("pk"), classifier_type="bothub", is_active=True)),
+    has_ia=Exists(
+        Classifier.objects.filter(
+            org=OuterRef("pk"), classifier_type="bothub", is_active=True
+        )
+    ),
     has_flows=Exists(Flow.objects.filter(org=OuterRef("pk"), is_active=True)),
     has_channel=Exists(Channel.objects.filter(org=OuterRef("pk"), is_active=True)),
     has_msg=Exists(Msg.objects.filter(org=OuterRef("pk"))),
+    has_channel_production=Exists(
+        Channel.objects.filter(org=OuterRef("pk"), is_active=True).exclude(
+            name="WhatsApp: +558231420933"
+        )
+    ),
 )
 
 
@@ -63,6 +72,7 @@ def get_success_orgs() -> "QuerySet[Org]":
                     has_flows=True,
                     has_channel=True,
                     has_msg=True,
+                    has_channel_production=True,
                     then=Value(True),
                 ),
                 output_field=BooleanField(),
@@ -79,7 +89,9 @@ def get_user_success_orgs(user: User) -> "QuerySet[Org]":
 def get_user_success_orgs_by_email(email: str) -> dict:
     user = get_user_by_email(email)
 
-    return dict(email=user.email, last_login=user.last_login, orgs=get_user_success_orgs(user))
+    return dict(
+        email=user.email, last_login=user.last_login, orgs=get_user_success_orgs(user)
+    )
 
 
 def retrieve_success_org(org_uuid: str) -> Org:
