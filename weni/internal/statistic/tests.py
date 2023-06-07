78fd41e128bb0ec2ce9e0c52afc01355a1a985e2
Lucas <44686243+lucaslinhares@users.noreply.github.com>
2023-06-07 18:05:10 -0300

    feat: Adjust Statistic app to new Project model. (#240)

--- weni/internal/statistic/tests.py
+++ weni/internal/statistic/tests.py
@@ -7,8 +7,13 @@ from django.contrib.auth.models import User
 
 from temba.api.models import APIToken
 from temba.flows.models import Flow
-from temba.orgs.models import Org
 from temba.tests import TembaTest
+from weni.internal.models import Project
+from weni.internal.statistic.views import StatisticEndpoint
+
+
+view = StatisticEndpoint
+view.permission_classes = []
 
 
 class TembaRequestMixin(ABC):
@@ -21,7 +26,7 @@ class TembaRequestMixin(ABC):
         return url
 
     def request_detail(self, uuid):
-        url = self.reverse(self.get_url_namespace(), kwargs={"uuid": uuid})
+        url = self.reverse(self.get_url_namespace(), kwargs={"project_uuid": uuid})
         token = APIToken.get_or_create(self.org, self.admin, Group.objects.get(name="Administrators"))
 
         return self.client.get(f"{url}", HTTP_AUTHORIZATION=f"Token {token.key}")
@@ -37,15 +42,15 @@ class StatisticTestCase(TembaTest, TembaRequestMixin):
 
     def test_retrieve(self):
         user = User.objects.create_user(username="testuser", password="123", email="test@weni.ai")
-        org = Org.objects.create(name="Temba", timezone="Africa/Kigali", created_by=user, modified_by=user)
+        project = Project.objects.create(name="Temba", timezone="Africa/Kigali", created_by=user, modified_by=user)
 
-        Flow.create(org=org, name="Flow test", user=user)
+        Flow.create(org=project.org, name="Flow test", user=user)
 
-        active_flows = org.flows.filter(is_active=True, is_archived=False).exclude(is_system=True).count()
-        active_classifiers = org.classifiers.filter(is_active=True).count()
-        active_contacts = org.contacts.filter(is_active=True).count()
+        active_flows = project.flows.filter(is_active=True, is_archived=False).exclude(is_system=True).count()
+        active_classifiers = project.classifiers.filter(is_active=True).count()
+        active_contacts = project.contacts.filter(is_active=True).count()
 
-        statistic_request = self.request_detail(uuid=str(org.uuid)).json()
+        statistic_request = self.request_detail(uuid=str(project.project_uuid)).json()
 
         self.assertEqual(statistic_request["active_flows"], active_flows)
         self.assertEqual(statistic_request["active_classifiers"], active_classifiers)
