75ce922858be8213aa880359086ccb65ec3c5a04
Lucas <44686243+lucaslinhares@users.noreply.github.com>
2023-06-22 18:53:31 -0300

    Adjust Flows app to new Project model. (#239)
    * feat: Adjust Flows app to new Project model.
    
    * change modelSerializer to Serializer

--- weni/internal/flows/serializers.py
+++ weni/internal/flows/serializers.py
@@ -1,35 +1,33 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
-from weni.grpc.core import serializers as weni_serializers
-from temba.flows.models import Flow
-
+from weni.serializers import fields as weni_fields
 
 User = get_user_model()
 
 
-class FlowSerializer(serializers.ModelSerializer):
-    org = weni_serializers.OrgUUIDRelatedField()
+class FlowSerializer(serializers.Serializer):
+    project = weni_fields.ProjectUUIDRelatedField(required=True, write_only=True)
     sample_flow = serializers.JSONField(write_only=True)
+    uuid = serializers.UUIDField(read_only=True)
 
     class Meta:
-        model = Flow
-        fields = ("org", "uuid", "sample_flow")
+        fields = ("project", "uuid", "sample_flow")
 
     def create(self, validated_data):
-        org = validated_data.get("org")
+        project = validated_data.get("project")
         sample_flows = validated_data.get("sample_flow")
-        org.import_app(sample_flows, org.created_by)
-        self.disable_flows_has_issues(org, sample_flows)
-        return org.flows.order_by("created_on").last()
+        project.import_app(sample_flows, project.created_by)
+        self.disable_flows_has_issues(project, sample_flows)
+        return project.flows.order_by("created_on").last()
 
-    def disable_flows_has_issues(self, org, sample_flows):
+    def disable_flows_has_issues(self, project, sample_flows):
         flows_name = list(map(lambda flow: flow.get("name"), sample_flows.get("flows")))
-        org.flows.filter(name__in=flows_name).update(has_issues=False)
+        project.flows.filter(name__in=flows_name).update(has_issues=False)
 
 
 class FlowListSerializer(serializers.Serializer):
     flow_name = serializers.CharField(required=True, write_only=True)
-    org_uuid = weni_serializers.OrgUUIDRelatedField(required=True, write_only=True)
+    project = weni_fields.ProjectUUIDRelatedField(required=True, write_only=True)
     uuid = serializers.CharField(read_only=True)
     name = serializers.CharField(read_only=True)
