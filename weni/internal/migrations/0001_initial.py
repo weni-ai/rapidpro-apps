582473028d1b5e97c4efdce8313788a168dfa24d
Rafael Soares <75167930+rasoro@users.noreply.github.com>
2023-08-24 18:24:11 -0300

    fix: clear dependencies from initial migration
    this `0025_remove_ticket_subject` dependency on initial migration is blocking Ilhasoft/rapidpro migrations

--- weni/internal/migrations/0001_initial.py
+++ weni/internal/migrations/0001_initial.py
@@ -7,9 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
     initial = True
 
-    dependencies = [
-        ("tickets", "0025_remove_ticket_subject"),
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
