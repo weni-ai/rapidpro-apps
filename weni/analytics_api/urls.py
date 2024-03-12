3409e026cc5524f7f4a58b0850e9840c3d86df10
lucaslinhares <lucaslinhares027@gmail.com>
2024-03-12 12:50:43 -0300

    Remove analytics flows call

--- weni/analytics_api/urls.py
+++ weni/analytics_api/urls.py
@@ -1,18 +1,13 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import ContactAnalyticsEndpoint, FlowRunAnalyticsEndpoint
+from .views import ContactAnalyticsEndpoint
 
 urlpatterns = [
     url(
         r"^analytics/contacts/$",
         ContactAnalyticsEndpoint.as_view(),
         name="api.v2.analytics.contacts",
-    ),
-    url(
-        r"^analytics/flow-runs/$",
-        FlowRunAnalyticsEndpoint.as_view(),
-        name="api.v2.analytics.flow_runs",
     ),
 ]
 
