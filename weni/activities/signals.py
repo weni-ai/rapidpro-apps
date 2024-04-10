27f4d01e6be9229748d85491e86f387a325251b6
Lucas <lucaslinhares027@gmail.com>
2024-04-10 17:18:21 -0300

    Add option to send delete in flow activity (#282)
    * Add option to send delete in flow activity
    
    * Add entity_uuid and project_uuid in recent activities publisher
    
    * Add testing condition to not call signal

--- weni/activities/signals.py
+++ weni/activities/signals.py
@@ -9,22 +9,38 @@ from temba.campaigns.models import Campaign
 from temba.event_driven.publisher.rabbitmq_publisher import RabbitmqPublisher
 
 
-def create_recent_activity(instance: models.Model, created: bool):
+def create_recent_activity(instance: models.Model, created: bool, delete=None):
     if instance.is_active:
-        action = "CREATE" if created else "UPDATE"
         rabbitmq_publisher = RabbitmqPublisher()
-        rabbitmq_publisher.send_message(
+        if delete:
+            action = "DELETE"
+            rabbitmq_publisher.send_message(
             body=dict(
                 action=action,
                 entity=instance.__class__.__name__.upper(),
                 entity_name=getattr(instance, "name", None),
+                entity_uuid=str(instance.uuid),
+                project_uuid=str(instance.org.project.project_uuid),
                 user=instance.modified_by.email,
                 flow_organization=str(instance.org.uuid),
             ),
             exchange="recent-activities.topic",
-            routing_key="",
+            routing_key="flow-delete",
         )
-
+        else:
+            action = "CREATE" if created else "UPDATE"
+            rabbitmq_publisher.send_message(
+                body=dict(
+                    action=action,
+                    entity=instance.__class__.__name__.upper(),
+                    entity_name=getattr(instance, "name", None),
+                    project_uuid=str(instance.org.project.project_uuid),
+                    user=instance.modified_by.email,
+                    flow_organization=str(instance.org.uuid),
+                ),
+                exchange="recent-activities.topic",
+                routing_key="",
+            )
 
 @receiver(post_save, sender=Channel)
 def channel_recent_activity_signal(sender, instance: Channel, created: bool, **kwargs):
