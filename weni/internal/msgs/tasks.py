0cc7ee1686cde4bacb2c669a4cb70899e98e248c
lucaslinhares <lucaslinhares027@gmail.com>
2024-12-26 18:17:18 -0300

    Using batch to avoid database time limit

--- weni/internal/msgs/tasks.py
+++ weni/internal/msgs/tasks.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import smtplib
 import logging
 
@@ -25,8 +26,8 @@ logger = logging.getLogger(__name__)
 def generate_sent_report_messages(**kwargs):
     org_id = kwargs.get("org_id")
     data = kwargs.get("data")
-    start_date = data["start_date"]
-    end_date = data["end_date"]
+    start_date = datetime.strptime(data["start_date"], "%Y-%m-%d")
+    end_date = datetime.strptime(data["end_date"], "%Y-%m-%d")
 
     logger.info(f"Starting report messages to org {org_id}")
 
@@ -40,31 +41,37 @@ def generate_sent_report_messages(**kwargs):
 
         logger.info(f"Starting task: generate_sent_report_messages for org {org_id}")
 
-        query = f"""
-            SELECT
-                template.name AS "Template",
-                flow.name AS "Fluxos Utilizados",
-                COUNT(msg.id) AS "Total por Template"
-            FROM
-                public.msgs_msg AS msg
-                INNER JOIN public.templates_template AS template
-                    ON template.name = msg.template
-                INNER JOIN public.flows_flow_template_dependencies AS depent
-                    ON depent.template_id = template.id
-                INNER JOIN public.flows_flow AS flow
-                    ON flow.id = depent.flow_id
-            WHERE
-                msg.created_on BETWEEN '{start_date}' AND '{end_date}'
-                AND msg.template IS NOT NULL
-                AND msg.org_id = {org_id}
-                AND msg.status IN ('S', 'D', 'V')
-            GROUP BY
-                template.name, flow.name
-            ORDER BY
-                COUNT(msg.id) DESC;
-        """
-        query_data = fetch_query_results(query)
-        processed_query_data = process_query_results(query_data)
+        time_batches = partition_date_range(start_date, end_date, timedelta(hours=4))
+        consolidated_data = []
+
+        for batch_start, batch_end in time_batches:
+            query = f"""
+                SELECT
+                    template.name AS "Template",
+                    flow.name AS "Fluxos Utilizados",
+                    COUNT(msg.id) AS "Total por Template"
+                FROM
+                    public.msgs_msg AS msg
+                    INNER JOIN public.templates_template AS template
+                        ON template.name = msg.template
+                    INNER JOIN public.flows_flow_template_dependencies AS depent
+                        ON depent.template_id = template.id
+                    INNER JOIN public.flows_flow AS flow
+                        ON flow.id = depent.flow_id
+                WHERE
+                    msg.created_on BETWEEN '{batch_start}' AND '{batch_end}'
+                    AND msg.template IS NOT NULL
+                    AND msg.org_id = {org_id}
+                    AND msg.status IN ('S', 'D', 'V')
+                GROUP BY
+                    template.name, flow.name
+                ORDER BY
+                    COUNT(msg.id) DESC;
+            """
+            query_data = fetch_query_results(query)
+            consolidated_data.extend(query_data)
+
+        processed_query_data = process_query_results(consolidated_data)
         file = export_data_to_excel(processed_query_data)
         send_report_file(file_stream=file, file_name="Mensagens Enviadas.xlsx", data=data)
         logger.info(f"Message Reports sent to org {org_id}")
@@ -74,6 +81,14 @@ def generate_sent_report_messages(**kwargs):
     finally:
         redis_client.delete(lock_key)
         logger.info(f"Lock released for org {org_id}")
+
+
+def partition_date_range(start_date, end_date, delta):
+    current_date = start_date
+    while current_date < end_date:
+        batch_end = min(current_date + delta, end_date)
+        yield current_date, batch_end
+        current_date = batch_end
 
 
 def fetch_query_results(query):
