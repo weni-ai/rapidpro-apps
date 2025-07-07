1bd61a356f933f05f50d3730cfbd29a4478343e3
Lucas <lucaslinhares027@gmail.com>
2025-07-07 17:28:43 -0300

    Publisher to channel event (#336)
    * Publisher to channel event
    
    * Convert uuid to str in publisher

---
+++ weni/internal/channel/publisher.py
@@ -0,0 +1,20 @@
+from temba.channels.models import Channel
+from temba.event_driven.publisher.rabbitmq_publisher import RabbitmqPublisher
+
+
+def publish_channel_event(channel: Channel, action: str):
+    
+    rabbitmq_publisher = RabbitmqPublisher()
+    
+    rabbitmq_publisher.send_message(
+        body=dict(
+            action=action,
+            uuid=channel.uuid,
+            project_uuid=str(channel.org.proj_uuid),
+            channel_type=channel.channel_type,
+            waba=channel.config.get("waba", None) if channel.channel_type == "WAC" else None,
+            phone_number=channel.config.get("phone_number", None) if channel.channel_type == "WAC" else None,
+        ),
+        exchange="channel-events.topic",
+        routing_key="",
+    )
